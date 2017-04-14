fdf270f5dcff9292425c6512c1de4312052562fb
chrisforrette <chris@chrisforrette.com>
2017-04-13 23:09:19 -0700

    Making Vimeo not force width/height to be an integer

--- shortcodes/parsers/vimeo.py
+++ shortcodes/parsers/vimeo.py
@@ -7,14 +7,8 @@ DEFAULT_CSS_CLASS = 'shortcode-vimeo'
 def parse(kwargs, template_name="shortcodes/vimeo.html"):
     video_id = kwargs.get('id')
     if video_id:
-        width = int(kwargs.get(
-            'width',
-            getattr(settings, 'SHORTCODES_VIMEO_WIDTH', 480))
-        )
-        height = int(kwargs.get(
-            'height',
-            getattr(settings, 'SHORTCODES_VIMEO_HEIGHT', 385))
-        )
+        width = getattr(settings, 'SHORTCODES_VIMEO_WIDTH', 480)
+        height = getattr(settings, 'SHORTCODES_VIMEO_HEIGHT', 385)
 
         ctx = {
             'css_class': getattr(settings, 'SHORTCODES_VIMEO_CSS_CLASS', DEFAULT_CSS_CLASS),
